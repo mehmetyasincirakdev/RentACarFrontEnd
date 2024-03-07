e939ff1429caa2a1bb40a8e1e94d1de3f74bbcb7
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-07 18:12:31 +0300

    first angular project and components created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'RentACar';
+  title: string = 'RentACar';
+  user: string = "Atakan Çetin";
+  
 }
