a785cb805dd16871ea33a7977ded1b8d9f6934ee
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-11 15:35:56 +0300

    A commit for ColorComponent that fetches data from colorService to retrieve colors.

---
+++ src/app/components/color/color.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ColorService } from '../../services/color.service';
+import { Color } from '../../models/color';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css'],
+})
+export class ColorComponent implements OnInit {
+  colors: Color[] = [];
+  
+  constructor(private colorService: ColorService) {}
+  ngOnInit(): void {
+    this.getColors();
+  }
+
+  getColors() {
+    this.colorService.getColors().subscribe((response) => {
+      this.colors = response.data;
+    });
+  }
+}
