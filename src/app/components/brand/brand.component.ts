fba4abaf56121591a084312ef432eb73f9cd9e1a
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-11 13:27:29 +0300

    added list group for brands

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrl: './brand.component.css'
+  styleUrl: './brand.component.css',
 })
 export class BrandComponent {
-
 }
