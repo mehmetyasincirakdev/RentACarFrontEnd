f2c77017dc0592e988d59249f74565cd7fde4a0b
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-11 15:33:04 +0300

    fixed brand listed operations

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrandService } from '../../services/brand.service';
+import { Brand } from '../../models/brand';
 
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
   styleUrl: './brand.component.css',
 })
-export class BrandComponent {
+export class BrandComponent implements OnInit {
+  brands: Brand[] = [];
+
+  constructor(private brandService: BrandService) {}
+  ngOnInit(): void {
+    this.getBrands();
+  }
+
+  getBrands() {
+    this.brandService.getBrands().subscribe((response) => {
+      this.brands = response.data;
+    });
+  }
 }
