c97cf20b67704fa11e5fb1a8d26c98db1fc6f609
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-11 15:34:52 +0300

    created color service

---
+++ src/app/services/color.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ColorResponseModel } from '../models/colorResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ColorService {
+  apiUrl = "http://localhost:61922/api/Colors/getall";
+  constructor(private httpClient: HttpClient) {}
+
+  getColors(): Observable<ColorResponseModel> {
+    return this.httpClient.get<ColorResponseModel>(this.apiUrl);
+  }
+}
