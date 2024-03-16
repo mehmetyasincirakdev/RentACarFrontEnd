6711a38536ba40c7ed07f6997f028909a1d7197b
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-16 03:00:28 +0300

    created rental component

---
+++ src/app/components/rental/rental.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { RentalService } from '../../services/rental.service';
+import { Rental } from '../../models/rental';
+
+@Component({
+  selector: 'app-rental',
+  templateUrl: './rental.component.html',
+  styleUrl: './rental.component.css',
+})
+export class RentalComponent implements OnInit {
+  rentals: Rental[] = [];
+  constructor(private rentalService: RentalService) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getRentals() {
+    this.rentalService
+      .getRentals()
+      .subscribe((response) => (this.rentals = response.data));
+  }
+}
