01f5de10bcd9a5945d8183206576588719569831
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-17 02:48:48 +0300

    The Observable structure has been made generic.

--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { BrandResponseModel } from '../models/brandResponseModel';
 import { HttpClient } from '@angular/common/http';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Brand } from '../models/brand';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class BrandService {
   apiUrl = 'http://localhost:61922/api/Brands/getall';
   constructor(private httpClient: HttpClient) {}
 
-  getBrands(): Observable<BrandResponseModel> {
-    return this.httpClient.get<BrandResponseModel>(this.apiUrl);
+  getBrands(): Observable<ListResponseModel<Brand>> {
+    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
   }
 }
